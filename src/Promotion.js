89706fe611114528ebbb25ab8b3287be33f8e9b8
The Coder <96771219+TheCoderAdhu@users.noreply.github.com>
2022-05-07 20:46:38 +0530

    Add files via upload

---
+++ src/Promotion.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Promotion() {
+  return (
+    <div className="alert alert-success" role="alert">
+      This Website is Made By Adhu. This website help use to manipulate the text
+      which has been put in the form😁😁.And it is made up of REACT JS took a
+      long time to learn this thing😴😴. And special thanks to this channel
+      (CodeWithHarry) who helped me to learn react js and some other things also
+      🙏🙏.
+    </div>
+  );
+}
+
+export default Promotion;
